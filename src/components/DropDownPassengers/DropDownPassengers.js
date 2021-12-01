1e6b57427129563ab7e0fd6a544276960e769f15
FrameMuse <minicablestone@gmail.com>
2021-12-01 17:07:46 +0300

    Dropdown Passangers refactoring

--- src/components/DropDownPassengers/DropDownPassengers.js
+++ src/components/DropDownPassengers/DropDownPassengers.js
@@ -1,12 +1,14 @@
+// SCSS
+import './passengers-list.scss'
+// ...
 import { passengerTypes, passengerClasses } from '../../constants';
 import DropDownPassengersItem from './DropDownPassengersItem';
 import DropDownPassengersClass from './DropDownPassengersClass';
 import React from "react";
-import './passengers-list.scss'
 
-function DropDownPassengers({ isPassengersOpen }) {
+function DropDownPassengers({ parentRef, isPassengersOpen }) {
   return (
-    <ul className={`passengers-list ${!isPassengersOpen ? "block-hidden" : ""}`}>
+    <ul className={`passengers-list ${!isPassengersOpen ? "passengers-list--hidden" : ""}`} ref={parentRef}>
       <li className="passengers-list__item">
         {passengerTypes.map(passenger =>
           <DropDownPassengersItem key={passenger.name} name={passenger.name} age={passenger.age}
