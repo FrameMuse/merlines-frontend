ea56d0bcb2dccef4c6bbe18b1a92c1276383110c
FrameMuse <minicablestone@gmail.com>
2021-12-04 02:33:48 +0300

    on the way to complete

--- src/components/DropDownCalendar/DropDownCalendarMonthDays.js
+++ src/components/DropDownCalendar/DropDownCalendarMonthDays.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import DropDownCalendarItem from "./DropDownCalendarItem"
+import { updateSearchCalendarDates } from "./DropDownCalendarReducer"
 
 function getAmountOfDays(date) {
   const newDate = new Date(date)
@@ -12,33 +13,51 @@ function getAmountOfDays(date) {
   return newDate.getDate()
 }
 
-function DropDownCalendarMonthDays({ date }) {
+function DropDownCalendarMonthDays({ currentDate }) {
+  const dispatch = useDispatch()
   const searchCalendar = useSelector(state => state.searchCalendar)
-  const [activeIndex, setActiveIndex] = useState(-1)
   const [enteredIndex, setEnteredIndex] = useState(-1)
   function shouldBeGrouped() {
-    if (activeIndex < 0) return false
     if (searchCalendar.mode === "single") return false
+    if (searchCalendar.dates.first == null) return false
 
     return true
+  }
+  function isActive(index) {
+    const comparedDate = new Date(currentDate)
+    comparedDate.setDate(index)
+
+    if (Object.values(searchCalendar.dates).some(date => date?.getTime() === comparedDate.getTime())) {
+      return true
+    }
+
+    return false
   }
   function isGrouped(index) {
     if (!shouldBeGrouped()) return false
 
     if (enteredIndex === index) return true
-    if (enteredIndex > index && activeIndex < index) return true
-    if (activeIndex > index && enteredIndex < index) return true
+    // if (enteredIndex > index && activeIndex < index) return true
+    // if (activeIndex > index && enteredIndex < index) return true
 
     return false
   }
+
+  function updateDate(dayIndex) {
+    const first = new Date(currentDate)
+    first.setDate(dayIndex)
+
+    dispatch(updateSearchCalendarDates({ first }))
+  }
+  console.log(searchCalendar.dates)
   return (
     <div className="drop-down-calendar__days-list">
-      {[...Array(getAmountOfDays(date))].map((_, index) => (
+      {[...Array(getAmountOfDays(currentDate))].map((_, index) => (
         <DropDownCalendarItem
           day={index + 1}
-          active={index === activeIndex}
+          active={isActive(index)}
           grouped={isGrouped(index)}
-          onClick={() => setActiveIndex(index)}
+          onClick={() => updateDate(index)}
           onPointerEnter={() => setEnteredIndex(index)}
           key={"item_" + index}
         />
