76d5a6b5c6a2f0a76c87e0c176c2684add327c2f
FrameMuse <minicablestone@gmail.com>
2021-12-04 01:01:31 +0300

    Рефакторинг DropDownCalendarMonthDays

--- src/components/DropDownCalendar/DropDownCalendarMonthDays.js
+++ src/components/DropDownCalendar/DropDownCalendarMonthDays.js
@@ -1,16 +1,43 @@
 import { useState } from "react"
+import { useSelector } from "react-redux"
 import DropDownCalendarItem from "./DropDownCalendarItem"
 
-function DropDownCalendarMonthDays() {
+function getAmountOfDays(date) {
+  const newDate = new Date(date)
+
+  newDate.setDate(1)
+  newDate.setMonth(newDate.getMonth() + 1)
+  newDate.setDate(0)
+
+  return newDate.getDate()
+}
+
+function DropDownCalendarMonthDays({ date }) {
+  const searchCalendar = useSelector(state => state.searchCalendar)
   const [activeIndex, setActiveIndex] = useState(-1)
   const [enteredIndex, setEnteredIndex] = useState(-1)
+  function shouldBeGrouped() {
+    if (activeIndex < 0) return false
+    if (searchCalendar.mode === "single") return false
+
+    return true
+  }
+  function isGrouped(index) {
+    if (!shouldBeGrouped()) return false
+
+    if (enteredIndex === index) return true
+    if (enteredIndex > index && activeIndex < index) return true
+    if (activeIndex > index && enteredIndex < index) return true
+
+    return false
+  }
   return (
     <div className="drop-down-calendar__days-list">
-      {[...Array(31)].map((_, index) => (
+      {[...Array(getAmountOfDays(date))].map((_, index) => (
         <DropDownCalendarItem
-          index={index + 1}
+          day={index + 1}
           active={index === activeIndex}
-          grouped={activeIndex > 0 && ((enteredIndex > index && activeIndex < index) || (activeIndex > index && enteredIndex < index) || (index === enteredIndex))}
+          grouped={isGrouped(index)}
           onClick={() => setActiveIndex(index)}
           onPointerEnter={() => setEnteredIndex(index)}
           key={"item_" + index}
