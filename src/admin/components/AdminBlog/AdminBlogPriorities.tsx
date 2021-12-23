5b34c7c2f87514513c59ab270b98bd6ee266df1d
FrameMuse <minicablestone@gmail.com>
2021-12-23 14:51:55 +0300

    Добавить управление рассылкой #40

---
+++ src/admin/components/AdminBlog/AdminBlogPriorities.tsx
@@ -0,0 +1,14 @@
+import "./AdminBlogPriorities.style.scss"
+
+import AdminSectionLayout from "admin/layouts/AdminSectionLayout"
+
+
+function AdminBlogPriorities() {
+  return (
+    <AdminSectionLayout header="Настрока Приоритетов">
+
+    </AdminSectionLayout>
+  )
+}
+
+export default AdminBlogPriorities
