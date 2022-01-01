bbe75bccd00340b3f585e307020dffc6617f1c53
FrameMuse <minicablestone@gmail.com>
2022-01-01 15:19:43 +0300

    add Icon suggestions to name

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -1,6 +1,8 @@
 import { SVGAttributes } from "react"
 
-interface IconProps extends SVGAttributes<SVGElement> { }
+interface IconProps extends SVGAttributes<SVGElement> {
+  name: ("arrow" | "baggage" | "baggageLg" | "bus" | "calendar" | "checkbox" | "close" | "departures" | "download" | "edit" | "facebook" | "filter" | "germany" | "history" | "instagram" | "like" | "loading" | "logout" | "notice" | "plane" | "question" | "russia" | "s7" | "search" | "share" | "star" | "switch" | "telegram" | "train" | "transfer" | "visibility" | "vkontakte" | "merlines" | "MerLines5" | "MerLines51" | "MerLines52" | "MerLines4" | "merlines1" | "merlines2" | "merlines3") | ({} & string)
+}
 
 function Icon(props: IconProps) {
   return (
