ffd541aa63e43307076bcd1d425ae419e51174c8
FrameMuse <minicablestone@gmail.com>
2022-03-04 17:17:57 +0300

    SearchResultWeekPriceCard patch

--- src/components/SearchResult/SearchResultWeekPrice/SearchResultWeekPriceCard.tsx
+++ src/components/SearchResult/SearchResultWeekPrice/SearchResultWeekPriceCard.tsx
@@ -1,3 +1,4 @@
+import { humanizeDate } from "components/SearchForm/SearchForm.utils"
 import { classWithModifiers } from "utils"
 
 
@@ -7,10 +8,11 @@ interface SearchResultWeekPriceCardProps {
 }
 
 function SearchResultWeekPriceCard(props: SearchResultWeekPriceCardProps) {
+  const date = new Date(props.date)
   return (
-    <div className={classWithModifiers("price-week__item", props.date === "20 Января, Пн" && "active")}>
-      <div className="price-week__price">{props.price}</div>
-      <div className="price-week__date">{props.date}</div>
+    <div className={classWithModifiers("price-week__item", date.getDay() === new Date().getDay() && "active")}>
+      <div className="price-week__price">{props.price.toPrice("ru", "rub")}</div>
+      <div className="price-week__date">{humanizeDate(date)}</div>
     </div>
   )
 }
