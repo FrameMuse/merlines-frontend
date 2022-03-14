957a749ef47b4f8ef8ef2650077b44c661a604a3
FrameMuse <minicablestone@gmail.com>
2022-03-14 17:35:47 +0300

    fix #199

--- src/components/SearchResult/SearchResultLoader.tsx
+++ src/components/SearchResult/SearchResultLoader.tsx
@@ -4,7 +4,7 @@ function SearchResultLoader() {
       <div className="download__inner">
         <h2 className="download__title">Подбираем билеты для Вас</h2>
       </div>
-      <div className="download__plane download__plane--closed-form"></div>
+      <div className="download__plane"></div>
     </section>
   )
 }
