1141d6cd5f4c1d46a9e3e271f09c0a31b893be38
Sanchik97 <sesancho97@gmail.com>
2022-03-30 16:36:53 +0500

    fix #184. Refactoring

--- src/components/UserAvatar/UserAvatar.tsx
+++ src/components/UserAvatar/UserAvatar.tsx
@@ -2,16 +2,18 @@ import "./UserAvatar.scss"
 
 import React from "react"
 
-interface props {
+interface UserAvatarProps {
   avatar: string
   firstName: string
-  size?:"small"
+  size?: "small"
 }
 
-const UserAvatar: React.FC<props> = ({avatar, firstName, size}) => {
+function UserAvatar(props: UserAvatarProps) {
   return (
-    <div className={["user-avatar", size ? `user-avatar--${size}` : ""].join(" ")}>
-      {avatar ? <img src={avatar} alt="avatar" className="nav__link-image"/> : firstName.substring(0, 1)}
+    <div className={["user-avatar", props.size ? `user-avatar--${props.size}` : ""].join(" ")}>
+      {props.avatar ?
+        <img src={props.avatar} alt="avatar" className="nav__link-image"/> :
+        props.firstName.substring(0, 1)}
     </div>
   )
 }
