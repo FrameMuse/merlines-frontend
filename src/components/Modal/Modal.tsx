73bc3e15efd024a5d8e1294aa0854166db78d588
FrameMuse <minicablestone@gmail.com>
2022-04-14 21:52:59 +0300

    fix #224

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import "./Modal.scss"
 
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 
 interface props {
   visible: boolean
@@ -8,23 +8,23 @@ interface props {
   closeBtn?: boolean
 }
 
-const Modal: React.FC<props> = ({visible, closeBtn, children, onCancel}) => {
+const Modal: React.FC<props> = ({ visible, closeBtn, children, onCancel }) => {
 
   useEffect(() => {
     visible ? document.body.style.overflow = "hidden" : document.body.style.overflow = "initial"
   }, [visible])
 
   return (
-    <div className={["modal", visible ? "modal--show" : ""].join(" ")}>
-      <div className={"modal__backdrop"} onClick={onCancel}/>
-      <div className={"modal__body"}>
-        {closeBtn && <button className={"modal__close-btn"} onClick={onCancel}>
+    <div className={["m-popup", visible ? "m-popup--show" : ""].join(" ")}>
+      <div className={"m-popup__backdrop"} onClick={onCancel} />
+      <div className={"m-popup__body"}>
+        {closeBtn && <button className={"m-popup__close-btn"} onClick={onCancel}>
           закрыть
-          <svg className={"modal__close-btn-icon"} width="10" height="10">
-            <use href="img/sprite.svg#close"/>
+          <svg className={"m-popup__close-btn-icon"} width="10" height="10">
+            <use href="img/sprite.svg#close" />
           </svg>
         </button>}
-        <div className={"modal__content"}>
+        <div className={"m-popup__content"}>
           {children}
         </div>
       </div>
